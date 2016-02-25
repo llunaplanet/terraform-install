099812dcacc9a723199aa2c82e29222963e86bf9
Cameron <cam.parry@gmail.com>
2016-02-25 16:57:10 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ fi
 mkdir -p $HOME/terraform        
 export PATH=$PATH:$HOME/terraform
 
-# download terraform        
+# download terraform from latest       
 cd $HOME/terraform 
 wget https://releases.hashicorp.com/terraform/${WERCKER_TERRAFORM_INSTALL_VERSION}/terraform_${WERCKER_TERRAFORM_INSTALL_VERSION}_linux_amd64.zip
 unzip terraform_${WERCKER_TERRAFORM_INSTALL_VERSION}_linux_amd64.zip 
